d6777b42f7f0402c28d5ef8f112895d79b9fb109
Yu Sheng <ays03@bu.edu>
2025-07-08 13:43:56 -0400

    Implemented denseStat.py

---
+++ src/finch/galley/dense_stats.py
@@ -0,0 +1,18 @@
+from typing import Any, Iterable
+from .tensor_stats import TensorStats, TensorDef
+
+class DenseStats(TensorStats):
+    def __init__(self, tensor: Any, fields: Iterable[str]):
+        self.defn = TensorDef.from_tensor(tensor, fields)
+
+    def estimate_non_fill_values(self) -> float:
+        return 0
+
+    def mapjoin(self, op, *args: "DenseStats") -> "DenseStats":
+        return 0
+
+    def aggregate(self, op, fields: Iterable[str], arg: "DenseStats") -> "DenseStats":
+        return 0
+
+    def issimilar(self, other: "TensorStats") -> bool:
+        return 0
