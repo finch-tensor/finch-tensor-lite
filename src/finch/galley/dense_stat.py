62d481b21ae7f1c74efdfcf2e26a254f0abe70c4
Yu Sheng <ays03@bu.edu>
2025-07-26 15:41:03 -0400

    Fix

--- src/finch/galley/dense_stat.py
+++ src/finch/galley/dense_stat.py
@@ -4,6 +4,7 @@ from typing import Any, Self
 from .tensor_def import TensorDef
 from .tensor_stats import TensorStats
 
+
 class DenseStats(TensorStats):
     def __init__(self, tensor: Any, fields: Iterable[str]) -> None:
         self.tensordef = TensorDef.from_tensor(tensor, fields)
