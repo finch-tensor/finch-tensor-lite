6fc02d91e5e342b33604682d5c09eb8ebdbcb9ff
Willow Ahrens <willow.marie.ahrens@gmail.com>
2025-07-28 14:34:56 -0400

    fix

--- src/finch/compile/__init__.py
+++ src/finch/compile/__init__.py
@@ -1,10 +1,7 @@
-from .extent import Extent, ExtentFormat, dimension, extent
 from .lower import BufferizedNDArray, NotationCompiler, NotationContext
 
 __all__ = [
     "BufferizedNDArray",
-    "Extent",
-    "ExtentFormat",
     "NotationCompiler",
     "NotationContext",
     "dimension",
