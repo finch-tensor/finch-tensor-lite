3b1d7eb4a67c179bc793042ce186a994f8a7ee8c
Yu Sheng <ays03@bu.edu>
2025-08-13 15:31:53 -0400

    Made changes to vector structure to dc

--- src/finch/galley/dc_stats.py
+++ src/finch/galley/dc_stats.py
@@ -2,12 +2,15 @@ from dataclasses import dataclass
 from typing import FrozenSet, Iterable, Any
 import numpy as np
 
+import finch.finch_notation as ntn
+from finch.compile import dimension
 from finch.finch_notation.nodes import (
     Variable, Literal, Call, Access, Read, Update, Declare, Thaw, Freeze,
     Loop, If, Increment, Unwrap, Block, Slot, Unpack, Repack
 )
+from finch.finch_notation.interpreter import NotationInterpreter
 
-from operator import add, ne
+import operator
 
 from .tensor_def import TensorDef
 from .tensor_stats import TensorStats
@@ -41,36 +44,58 @@ class DCStats(TensorStats):
             raise NotImplementedError(f"DC analysis not implemented for {ndim}D tensors")
 
     def _vector_structure_to_dcs(self) -> set[DC]:
+        """
+        Build a Finch Notation program that counts non-fill entries in a 1-D tensor,
+        execute it, and return a set of DC.
+        """
+        A  = ntn.Variable("A", np.ndarray)
+        A_ = ntn.Slot("A_", np.ndarray)
 
-        A_slot = Slot("A", type=None)
-        A_unpack = Unpack(A_slot, Literal(self.tensor) )
+        d  = ntn.Slot("d", np.ndarray)
+        i  = ntn.Variable("i", np.int64)
+        fill = ntn.Literal(self.tensordef.fill_value)
 
-        B_slot = Slot("B", type=None)
-        B_declare = Declare(B_slot, Literal(0.0), Literal(add), ())
-        B_thaw = Thaw(B_slot, Literal(add))
+        prgm = ntn.Module((
+            ntn.Function(
+                ntn.Variable("count_nonfill", np.int64),
+                (A,),
+                ntn.Block((
+                    ntn.Unpack(A_, A),
+                    ntn.Declare(d, ntn.Literal(0.0), ntn.Literal(operator.add), ()),
+                    ntn.Thaw(d, ntn.Literal(operator.add)),
 
-        body = If(
-            Call(Literal(ne), Access(A_slot, Read(), Variable("i")), Literal(0.0)),
-            Increment(Access(B_slot, Update(Literal(add)), ()), Literal(1.0))
-        )
-        loop = Loop(Variable("i"), Literal(self.tensor.shape[0]), body)
+                    ntn.Loop(
+                        i,
+                        ntn.Literal(int(self.tensor.shape[0])),
+                        ntn.Block((
+                            ntn.If(
+                                ntn.Call(
+                                    ntn.Literal(operator.ne),
+                                    (
+                                        ntn.Unwrap(ntn.Access(A_, ntn.Read(), (i,))),
+                                        fill,
+                                    ),
+                                ),
+                                ntn.Increment(
+                                    ntn.Access(d, ntn.Update(ntn.Literal(operator.add)), ()),
+                                    ntn.Literal(1.0),
+                                ),
+                            ),
+                        )),
+                    ),
 
-        B_freeze = Freeze(B_slot, Literal(add))
-        result = Unwrap(B_slot)
+                    ntn.Freeze(d, ntn.Literal(operator.add)),
+                    ntn.Return(ntn.Unwrap(d)),
+                    ntn.Repack(A_, A),
+                )),
+            ),
+        ))
 
-        A_repack = Repack(A_slot, Literal(self.tensor))
+        mod = ntn.NotationInterpreter()(prgm)
+        cnt = mod.count_nonfill(np.asarray(self.tensor))
+        result = self.fields[0]
 
-        program = Block.from_children(
-            A_unpack,
-            B_declare,
-            B_thaw,
-            loop,
-            B_freeze,
-            result,
-            A_repack,
-        )
-        return program
-
+        return {DC(frozenset(), frozenset([result]), float(cnt))}
 
     @staticmethod
     def mapjoin(op, *args, **kwargs):
