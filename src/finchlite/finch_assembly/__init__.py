bf568239e101375799cec9d63791ded2c1341b7c
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-09-26 23:04:33 -0400

    refac: add new clases to __init__.py
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>

--- src/finchlite/finch_assembly/__init__.py
+++ src/finchlite/finch_assembly/__init__.py
@@ -1,4 +1,5 @@
 from .buffer import Buffer, BufferFType, element_type, length_type
+from .cfg_builder import CFGBuilder
 from .interpreter import AssemblyInterpreter, AssemblyInterpreterKernel
 from .nodes import (
     AssemblyNode,
@@ -47,6 +48,7 @@ __all__ = [
     "Buffer",
     "BufferFType",
     "BufferLoop",
+    "CFGBuilder",
     "Call",
     "ForLoop",
     "Function",
