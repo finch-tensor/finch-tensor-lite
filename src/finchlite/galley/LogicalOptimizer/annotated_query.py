01db1205588ba87c3fa29ea2df18a28341509d53
Yu Sheng <ays03@bu.edu>
2025-10-29 01:41:10 -0400

    Pre-commit

--- src/finchlite/galley/LogicalOptimizer/annotated_query.py
+++ src/finchlite/galley/LogicalOptimizer/annotated_query.py
@@ -9,26 +9,24 @@ from finchlite.finch_logic import LogicNode
 class AnnotatedQuery:
     ST: type
     output_name: Any
-    output_order: list[str] | None = None
-    output_format: list[Any] | None = None
     reduce_idxs: list[str]
     point_expr: "LogicNode"
     idx_lowest_root: OrderedDict[str, int]
     idx_op: OrderedDict[str, Any]
     idx_init: OrderedDict[str, Any]
-    id_to_node: OrderedDict[int, LogicNode]
+    id_to_node: OrderedDict[int, "LogicNode"]
     parent_idxs: OrderedDict[str, list[str]]
     original_idx: OrderedDict[str, str]
     connected_components: list[list[str]]
     connected_idxs: OrderedDict[str, set[str]]
+    output_order: list[str] | None = None
+    output_format: list[Any] | None = None
 
 
 def copy_aq(aq: AnnotatedQuery) -> AnnotatedQuery:
     return AnnotatedQuery(
         ST=aq.ST,
         output_name=aq.output_name,
-        output_order=None if aq.output_order is None else list(aq.output_order),
-        output_format=None if aq.output_format is None else list(aq.output_format),
         reduce_idxs=list(aq.reduce_idxs),
         point_expr=aq.point_expr,
         idx_lowest_root=OrderedDict(aq.idx_lowest_root.items()),
@@ -39,6 +37,8 @@ def copy_aq(aq: AnnotatedQuery) -> AnnotatedQuery:
         original_idx=OrderedDict(aq.original_idx.items()),
         connected_components=[list(n) for n in aq.connected_components],
         connected_idxs=OrderedDict((m, set(n)) for m, n in aq.connected_idxs.items()),
+        output_order=None if aq.output_order is None else list(aq.output_order),
+        output_format=None if aq.output_format is None else list(aq.output_format),
     )
 
 
