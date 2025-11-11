bf50514d6d335dfb2ad83d3caf5c85310648caea
Yu Sheng <ays03@bu.edu>
2025-11-11 11:16:54 -0500

    pre-commit

--- tests/test_galley.py
+++ tests/test_galley.py
@@ -20,7 +20,12 @@ from finchlite.galley.LogicalOptimizer.annotated_query import (
     get_reducible_idxs,
 )
 from finchlite.galley.LogicalOptimizer.logic_to_stats import _insert_statistics
-from finchlite.galley.LogicalOptimizer.utility import PostOrderDFS, PreOrderDFS, intree, isdescendant
+from finchlite.galley.LogicalOptimizer.utility import (
+    PostOrderDFS,
+    PreOrderDFS,
+    intree,
+    isdescendant,
+)
 from finchlite.galley.TensorStats.dc_stats import DC, DCStats
 from finchlite.galley.TensorStats.dense_stat import DenseStats
 from finchlite.galley.TensorStats.tensor_def import TensorDef
