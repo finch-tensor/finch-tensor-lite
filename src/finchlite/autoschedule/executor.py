24a612cfb84fa606c0a04d9dafacbb20579a8c06
Willow Ahrens <willow.marie.ahrens@gmail.com>
2025-11-27 19:35:55 -0500

    new executor

--- src/finchlite/autoschedule/executor.py
+++ src/finchlite/autoschedule/executor.py
@@ -1,26 +1,142 @@
-from ..symbolic import gensym
+from typing import overload
+
+from .. import finch_logic as lgc
+from ..finch_logic import (
+    LogicEvaluator,
+    LogicLoader,
+    LogicNode,
+)
+from ..symbolic import Namespace, PostWalk, Rewrite, ftype
 from .compiler import LogicCompiler
 
 
-class LogicExecutor:
-    def __init__(self, ctx: LogicCompiler, verbose: bool = False):
-        self.ctx: LogicCompiler = ctx
-        self.codes: dict = {}
+def extract_tables(
+    root: LogicNode,
+    bindings: dict[lgc.Alias, lgc.TableValue],
+) -> tuple[lgc.LogicNode, dict[lgc.Alias, lgc.TableValue]]:
+    """
+    Extracts tables from logic plan, replacing them with aliases.
+    """
+    bindings = bindings.copy()
+    ids: dict[int, lgc.Alias] = {id(val.tns): key for key, val in bindings.items()}
+    spc = Namespace(root)
+    for alias in bindings:
+        spc.freshen(alias.name)
+
+    def rule_0(node):
+        match node:
+            case lgc.Table(tns, idxs):
+                if not isinstance(tns, lgc.Literal):
+                    raise ValueError(f"Table tns must be Literal, got {tns}")
+                tns = tns.val
+                if id(tns) in ids:
+                    var = ids[id(tns)]
+                    if bindings[var].idxs == idxs:
+                        return var
+                    return lgc.Relabel(var, idxs)
+                var = lgc.Alias(spc.freshen("A"))
+                ids[id(tns)] = var
+                bindings[var] = lgc.TableValue(tns, idxs)
+                return var
+
+    root = Rewrite(PostWalk(rule_0))(root)
+    return root, bindings
+
+
+class LogicFieldsContext:
+    def __init__(self, bindings: dict[lgc.Alias, tuple[lgc.Field, ...]] | None = None):
+        if bindings is None:
+            bindings = {}
+        self.bindings: dict[lgc.Alias, tuple[lgc.Field, ...]] = bindings
+
+    @overload
+    def __call__(self, node: lgc.LogicExpression) -> tuple[lgc.Field, ...]: ...
+    @overload
+    def __call__(
+        self, node: lgc.LogicStatement
+    ) -> tuple[tuple[lgc.Field, ...], ...]: ...
+    @overload
+    def __call__(
+        self, node: lgc.LogicNode
+    ) -> tuple[lgc.Field, ...] | tuple[tuple[lgc.Field, ...], ...]: ...
+    def __call__(self, node):
+        match node:
+            case lgc.Alias(_):
+                if node not in self.bindings:
+                    raise ValueError(f"undefined tensor alias {node}")
+                return self.bindings[node]
+            case lgc.Table(_, idxs):
+                return idxs
+            case lgc.MapJoin(_, args):
+                args_idxs = [self(a) for a in args]
+                return tuple(dict.fromkeys([f for fs in args_idxs for f in fs]))
+            case lgc.Aggregate(_, _, arg, idxs):
+                arg_idxs = self(arg)
+                return tuple(idx for idx in arg_idxs if idx not in idxs)
+            case lgc.Relabel(arg, idxs):
+                return idxs
+            case lgc.Reorder(arg, idxs):
+                return idxs
+            case lgc.Subquery(lhs, arg):
+                res = self.bindings.get(lhs)
+                if res is None:
+                    res = self(arg)
+                    self.bindings[lhs] = res
+                return res
+            case lgc.Query(lhs, rhs):
+                rhs = self(rhs)
+                self.bindings[lhs] = rhs
+                return (rhs,)
+            case lgc.Plan(bodies):
+                res = ()
+                for body in bodies:
+                    res = self(body)
+                return res
+            case lgc.Produces(args):
+                return tuple(self(arg) for arg in args)
+            case _:
+                raise ValueError(f"Unknown expression type: {type(node)}")
+
+
+def get_return_fields(
+    prgm: lgc.LogicNode, bindings: dict[lgc.Alias, tuple[lgc.Field, ...]]
+):
+    ctx = LogicFieldsContext(bindings)
+    return ctx(prgm)
+
+
+class LogicExecutor(LogicEvaluator):
+    def __init__(self, ctx: LogicLoader | None = None, verbose: bool = False):
+        if ctx is None:
+            ctx = LogicCompiler()
+        self.ctx: LogicLoader = ctx
         self.verbose: bool = verbose
 
-    def __call__(self, prgm):
-        prgm_structure = prgm
-        if prgm_structure not in self.codes:
-            thunk = logic_executor_code(self.ctx, prgm)
-            self.codes[prgm_structure] = eval(thunk), thunk
+    def __call__(self, prgm, bindings: dict[lgc.Alias, lgc.TableValue] | None = None):
+        if bindings is None:
+            bindings = {}
+        prgm, bindings = extract_tables(prgm, bindings)
+        binding_ftypes = {var: ftype(val) for var, val in bindings.items()}
 
-        f, code = self.codes[prgm_structure]
-        if self.verbose:
-            print(code)
-        return f(prgm)
+        sizes = {
+            idx: dim
+            for tbl in bindings.values()
+            for (idx, dim) in zip(tbl.idxs, tbl.tns.shape, strict=True)
+        }
 
+        mod, workspace_ftypes = self.ctx(prgm, binding_ftypes)
 
-def logic_executor_code(ctx, prgm):
-    code = ctx(prgm)
-    fname = gensym("compute")
-    return f""":(function {fname}(prgm) \n {code} \n end)"""
+        inputs = [tbl.tns for tbl in bindings.values()]
+        workspaces = [
+            tbl.tns(sizes[idx] for idx in tbl.idxs) for tbl in workspace_ftypes.values()
+        ]
+
+        res = mod.main(*inputs, *workspaces)
+
+        res_ftype = get_return_fields(
+            prgm, {var: tbl.idxs for var, tbl in bindings.items()}
+        )
+
+        if isinstance(res, tuple):
+            return (t(tns) for t, tns in zip(res_ftype, res, strict=True))
+        return res_ftype(res)
