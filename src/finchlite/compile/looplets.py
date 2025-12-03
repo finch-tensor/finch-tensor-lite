107476360c073a728b500ae67da8445bd3960a4f
Mateusz Sokół <mat646@gmail.com>
2025-12-03 21:37:46 +0100

    LoTriMask tensor and Sequence looplet

--- src/finchlite/compile/looplets.py
+++ src/finchlite/compile/looplets.py
@@ -1,12 +1,15 @@
+import operator
 from abc import ABC, abstractmethod
 from collections.abc import Callable
 from dataclasses import dataclass
+from functools import partial
 from typing import Any
 
-from finchlite.compile.lower import LoopletPass, SingletonExtentFType
+import numpy as np
 
 from .. import finch_assembly as asm
 from .. import finch_notation as ntn
+from ..compile.lower import LoopletPass, SimpleExtentFType, SingletonExtentFType
 from ..symbolic import PostWalk
 
 
@@ -87,16 +90,76 @@ class SpikePass(LoopletPass):
 
 @dataclass
 class Sequence:
-    head: Any
-    split: Any
-    tail: Any
+    head: Callable
+    split: Callable
+    tail: Callable
+
+    @property
+    def pass_request(self):
+        return SequencePass()
 
 
 @dataclass
+class SetOnce:
+    obj: Any | None = None
+
+    def set(self, obj):
+        if self.obj is None:
+            self.obj = obj
+        elif self.obj != obj:
+            raise Exception(f"SetOnce object already set with: {self.obj}")
+
+    def get(self):
+        if self.obj is None:
+            raise Exception("The SetOnce object hasn't been set.")
+        return self.obj
+
+
 class SequencePass(LoopletPass):
     @property
     def priority(self):
-        return 0
+        return 1
+
+    def __call__(self, ctx, idx, visited_idxs, ext, body):
+        split_var = SetOnce()
+
+        def sequence_node(node, seq_field: str):
+            match node:
+                case ntn.Access(tns, mode, (j, *idxs)):
+                    if j == idx and isinstance(tns, Sequence):
+                        split_var.set(tns.split(ctx, idx, visited_idxs))
+                        return ntn.Access(
+                            getattr(tns, seq_field)(ctx, idx), mode, (j, *idxs)
+                        )
+            return None
+
+        # process head
+        sequence_head = partial(sequence_node, seq_field="head")
+        body_head = PostWalk(sequence_head)(body)
+        ext_2 = SimpleExtentFType(
+            ext.result_format.get_start(ext),
+            asm.Call(
+                asm.Literal(operator.add), (split_var.get(), asm.Literal(np.intp(1)))
+            ),
+        )
+        ctx_2 = ctx.scope()
+        ctx_2(ext_2, body_head)
+        emitted_head = ctx_2.emit()
+
+        # process tail
+        sequence_tail = partial(sequence_node, seq_field="tail")
+        body_tail = PostWalk(sequence_tail)(body)
+        ext_3 = SimpleExtentFType(
+            asm.Call(
+                asm.Literal(operator.add), (split_var.get(), asm.Literal(np.intp(2)))
+            ),
+            ext.result_format.get_end(ext),
+        )
+        ctx_3 = ctx.scope()
+        ctx_3(ext_3, body_tail)
+        emitted_tail = ctx_3.emit()
+
+        ctx.exec(asm.Block((*emitted_head, *emitted_tail)))
 
 
 @dataclass
@@ -111,7 +174,20 @@ class Run:
 class RunPass(LoopletPass):
     @property
     def priority(self):
-        return 0
+        return 1
+
+    def __call__(self, ctx, idx, visited_idxs, ext, body):
+        def run_node(node):
+            match node:
+                case ntn.Access(tns, mode, (j, *idxs)):
+                    if j == idx and isinstance(tns, Run):
+                        return ntn.Access(tns.body(ctx), mode, (j, *idxs))
+            return None
+
+        # TODO: Proper Run Pass
+        PostWalk(run_node)(body)
+        ctx_2 = ctx.scope()
+        ctx.exec(asm.Block(ctx_2.emit()))
 
 
 @dataclass
@@ -148,7 +224,7 @@ class LookupPass(LoopletPass):
     def priority(self):
         return 0
 
-    def __call__(self, ctx, idx, ext, body):
+    def __call__(self, ctx, idx, visited_idxs, ext, body):
         idx_2 = asm.Variable(ctx.freshen(idx.name), idx.result_format)
 
         def lookup_node(node):
@@ -166,7 +242,7 @@ class LookupPass(LoopletPass):
         ctx_2 = ctx.scope()
         ext_2 = SingletonExtentFType.stack(idx_2)
         ctx_2(ext_2, body_2)
-        start = ext.result_format.get_start(ext)
+        start = ext.result_format.get_start(ext)  # TODO: We should accept arbitrary ext
         stop = ext.result_format.get_end(ext)
         body_3 = asm.Block(ctx_2.emit())
         ctx.exec(asm.ForLoop(idx_2, start, stop, body_3))
@@ -206,9 +282,9 @@ class Leaf:
 class LeafPass(LoopletPass):
     @property
     def priority(self):
-        return 2
+        return 0
 
-    def __call__(self, ctx, idx, ext, body):
+    def __call__(self, ctx, idx, visited_idxs, ext, body):
         def leaf_node(node):
             match node:
                 case ntn.Access(tns, mode, (j, *idxs)):
@@ -217,4 +293,4 @@ class LeafPass(LoopletPass):
             return None
 
         body_2 = PostWalk(leaf_node)(body)
-        ctx.ctx(body_2)
+        ctx.ctx(body_2, visited_idxs + (idx,))  # calling NotationContext
---
+++ src/finchlite/tensor/masks.py
@@ -0,0 +1,152 @@
+from dataclasses import dataclass, field
+from typing import Any
+
+import numpy as np
+
+from .. import finch_assembly as asm
+from .. import finch_notation as ntn
+from ..algebra import Tensor
+from ..compile import looplets as lplt
+from ..tensor import Level, LevelFType
+
+
+@dataclass(unsafe_hash=True)
+class LoTriMaskFType(LevelFType, asm.AssemblyStructFType):
+    body: LevelFType
+
+    @property
+    def element_type(self):
+        return self.body.element_type
+
+    @property
+    def buffer_type(self):
+        return self.body.buffer_type
+
+    @property
+    def position_type(self):
+        return self.body.position_type
+
+    @property
+    def fill_value(self):
+        return self.body.fill_value
+
+    @property
+    def shape_type(self):
+        return self.body.shape_type
+
+    @property
+    def buffer_factory(self):
+        return self.body.buffer_factory
+
+    @property
+    def ndim(self):
+        return self.body.ndim
+
+    @property
+    def lvl_t(self):
+        return self.body.lvl_t
+
+    def lower_freeze(self, ctx, tns, op):
+        return self.body.lower_freeze(self, ctx, tns, op)
+
+    def lower_thaw(self, ctx, tns, op):
+        return self.body.lower_thaw(self, ctx, tns, op)
+
+    def lower_unwrap(self, ctx, obj):
+        return self.body.lower_unwrap(self, ctx, obj)
+
+    def lower_increment(self, ctx, obj, val):
+        return self.body.lower_increment(self, ctx, obj, val)
+
+    def lower_declare(self, ctx, tns, init, op, shape):
+        return self.body.lower_declare(self, ctx, tns, init, op, shape)
+
+    def unfurl(self, ctx, tns, ext, mode, proto):
+        def child_accessor(ctx, idx):
+            return self.body.unfurl(ctx, tns, ext, mode, proto)
+
+        return lplt.Sequence(
+            head=lambda ctx, idx: child_accessor(ctx, idx),
+            split=lambda ctx, idx, visited_idx: ctx.ctx(visited_idx[-1]),
+            tail=lambda ctx, idx: lplt.Run(
+                lambda ctx: lplt.Leaf(
+                    lambda ctx: ntn.Stack(asm.Literal(np.intp(0)), np.intp)
+                )
+            ),
+        )
+
+    def from_kwargs(self, **kwargs):
+        return self.body.from_kwargs(**kwargs)
+
+    def to_kwargs(self):
+        return self.body.to_kwargs()
+
+    def asm_unpack(self, ctx, var_n, val):
+        val_body = asm.GetAttr(val, asm.Literal("body"))
+        return self.body.asm_unpack(ctx, var_n, val_body)
+
+    def asm_repack(self, ctx, lhs, obj):
+        return self.body.asm_repack(ctx, lhs, obj)
+
+    def get_fields_class(self, tns, buf_s, nind, pos, op):
+        return self.body.get_fields_class(tns, buf_s, nind, pos, op)
+
+    def __call__(self, *args, **kwargs):
+        return LoTriMask(self, self.body(*args, **kwargs))
+
+    @property
+    def struct_name(self):
+        return "LoTriMaskFType"
+
+    @property
+    def struct_fields(self):
+        return [("body", self.body)] + self.body.struct_fields
+
+
+@dataclass
+class LoTriMask(Level):
+    """
+    Lower triangular mask column level for storing column idx.
+    """
+
+    _format: LoTriMaskFType = field(repr=False)
+    lvl: Level
+
+    @property
+    def ftype(self) -> LoTriMaskFType:
+        return self._format
+
+    @property
+    def val(self) -> Any:
+        return self.lvl.val
+
+    @property
+    def shape(self):
+        return self.lvl.shape
+
+    @property
+    def stride(self):
+        return self.lvl.stride
+
+    @property
+    def dimension(self):
+        return self.lvl.dimension
+
+    @property
+    def body(self):
+        return self.lvl
+
+
+def tril(x: Tensor, /, *, k: int = 0) -> Tensor:
+    if k != 0:
+        raise Exception(f"Only k=0 is supported, but got: {k}")
+
+    # insert column level in appropriate place
+    lvl = x.lvl
+    for _ in range(x.ndim - 2):
+        lvl = lvl.lvl
+    low_tri_mask_column = LoTriMask(LoTriMaskFType(lvl.lvl.ftype), lvl.lvl)
+    lvl.lvl = low_tri_mask_column
+    lvl._format._lvl_t = low_tri_mask_column.ftype
+
+    return x
