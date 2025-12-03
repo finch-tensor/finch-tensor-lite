88732b435ea743114bcc4b9571d8afc96968ddfe
Juni Kim <junikimm717@gmail.com>
2025-12-02 20:08:44 -0500

    we will try putting the int only struct first

--- tests/test_codegen.py
+++ tests/test_codegen.py
@@ -1067,13 +1067,13 @@ def test_multiple_c_hashtable(compiler):
 
     # what's important here is that you can call setidx_1 on table1 and
     # setidx_2 on table2.
-    assert mod.setidx_1(
-        table1, table1.key_type(1, 2.0), table1.value_type(2, 3, 4)
-    ) == table1.value_type(2, 3, 4)
-
     assert mod.setidx_2(
         table2, table2.key_type(1), table2.value_type(2, 3, 4, 5)
     ) == table2.value_type(2, 3, 4, 5)
+
+    assert mod.setidx_1(
+        table1, table1.key_type(1, 2.0), table1.value_type(2, 3, 4)
+    ) == table1.value_type(2, 3, 4)
 
     assert mod.setidx_3(
         table3, table3.key_type(1, 2.0), table3.value_type(1.434, 1.4)
