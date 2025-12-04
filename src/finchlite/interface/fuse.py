5801a9c4a076d324fffade71255237a70262ca26
Mateusz Sokół <mat646@gmail.com>
2025-12-04 15:34:43 +0100

    ruff

--- src/finchlite/interface/fuse.py
+++ src/finchlite/interface/fuse.py
@@ -53,10 +53,10 @@ Performance:
 from enum import Enum
 from typing import Any
 
+import numpy as np
+
 from finchlite.autoschedule import LogicExecutor, LogicNormalizer
 from finchlite.finch_logic.nodes import TableValue
-
-import numpy as np
 
 from .. import finch_assembly as asm
 from .. import finch_notation as ntn
@@ -191,7 +191,9 @@ def provision_tensors(
             case Table(Literal(val), idxs):
                 if isinstance(val, TensorPlaceholder):
                     shape = tuple(dims_dict[field] for field in idxs)
-                    tensor = table_var.type_(shape, val=np.zeros(dtype=val.dtype, shape=shape))
+                    tensor = table_var.type_(
+                        shape, val=np.zeros(dtype=val.dtype, shape=shape)
+                    )
                 else:
                     for idx, field in enumerate(table.idxs):
                         dims_dict[field] = val.shape[idx]
