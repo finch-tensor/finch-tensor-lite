3b6d4dd3b0e3c17225bdcee522d45e2ed0a1cc5e
kylebd99 <kylebd99@gmail.com>
2026-02-17 11:02:28 -0500

    add stats_interpreter & remove unnecessary exports

--- src/finchlite/galley/TensorStats/__init__.py
+++ src/finchlite/galley/TensorStats/__init__.py
@@ -1,41 +1,16 @@
-import finchlite
-from finchlite.finch_einsum.nodes import (
-    Access,
-    Alias,
-    Call,
-    Einsum,
-    EinsumExpression,
-    EinsumNode,
-    EinsumStatement,
-    Index,
-    Literal,
-    Plan,
-    Produces,
-)
-
 from .dc_stats import DC, DCStats
 from .dense_stat import DenseStats
+from .stats_interpreter import StatsInterpreter
 from .tensor_def import TensorDef
 from .tensor_stats import TensorStats
 from .uniform_stats import UniformStats
 
 __all__ = [
     "DC",
-    "Access",
-    "Alias",
-    "Call",
     "DCStats",
     "DenseStats",
-    "Einsum",
-    "EinsumExpression",
-    "EinsumNode",
-    "EinsumStatement",
-    "Index",
-    "Literal",
-    "Plan",
-    "Produces",
+    "StatsInterpreter",
     "TensorDef",
     "TensorStats",
     "UniformStats",
-    "finchlite",
 ]
