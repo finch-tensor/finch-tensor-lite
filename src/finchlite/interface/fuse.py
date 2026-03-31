259703ac33cfdc2996dbeeea899d4e5e17c8929c
kylebd99 <kylebd99@gmail.com>
2026-03-31 10:24:53 -0400

    use assembly interpreter to speed up galley

--- src/finchlite/interface/fuse.py
+++ src/finchlite/interface/fuse.py
@@ -126,12 +126,12 @@ COMPILE_NUMBA = LogicNormalizer(
 
 # TODO: Make Galley a LogicLoader that gets passed a stats bindings dictionary
 # rather than the tensors themselves.
-INTERPRET_NOTATION_GALLEY = LogicNormalizer(
+INTERPRET_ASSEMBLY_GALLEY = LogicNormalizer(
     GalleyLogicalOptimizer(
         DenseStats,
         LogicExecutor(
             LogicStandardizer(
-                DefaultLogicFormatter(LogicCompiler(NotationInterpreter()))
+                DefaultLogicFormatter(LogicCompiler(NotationCompiler(AssemblyInterpreter())))
             )
         ),
     )
