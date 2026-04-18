4ac9ebb2e35f732eed041e57525cc5aed6f9d4df
idkwhttsay <danil.tatarinov.00@gmail.com>
2026-04-18 17:37:39 -0400

    fix pre-commit

--- tests/test_notation_dataflow.py
+++ tests/test_notation_dataflow.py
@@ -1,5 +1,7 @@
-from .scripts.nodes import create_ntn_simple_node
 from finchlite.finch_notation import NotationCFGBuilder
+
+from .scripts.nodes import create_ntn_simple_node
+
 
 def test_ntn_cfg_printer_simple(file_regression):
     prgm = create_ntn_simple_node()
