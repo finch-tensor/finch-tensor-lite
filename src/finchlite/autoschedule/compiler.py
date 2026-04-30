c94b1fe8cb422b489e9499c2a8cf725ba6916dc1
Joel Mathew Cherian <joelmathewcherian@gmail.com>
2026-04-30 11:12:46 -0400

    autoscheduler: efficiently support in-place updates in finch-logic (#386)
    * fix: bug with standardize:split_increments that where rhs of a query was a alias instead of a table
    
    * feat: added match case for query containing reordered mapjoin over aggregates
    
    * feat: updated the notation context to support lowering from new canonical form
    
    * chore: fixing mypy issues
    
    * feat: improved split_increments in the logic standardizer to prevent it from splitting on inplace updates
    
    * chore: refactoring is_inplace_expr
    
    * feat: standardizer now allows canonical form 3
    
    * fix: corrected generated finch-ntn by adding thaw
    
    * feat: addressing PR comment
    
    * chore: fixed tests
    
    * wip: added inplace standardize to make lhs_arg the first one
    
    * cleanup
    
    * chore: fixing tests
    
    * feat: got test working
    
    * feat: got the whole thing working
    
    * fix: made unflattening a seperate walk
    
    * resolved bugs with tests
    
    * fix: resolving mypy issues
    
    * chore: correcting some incorrect merge code
    
    * chore: revert non_lhs_arg
    
    * feat: moved is_inplace_expr into match pattern
    
    * feat: completed merging inplace conditions into match pattern
    
    * feat: corrected the op check to ensure it includes overwrite for cases where A[i,j] + B[i,j]
    
    * feat: extending drop reorders to recognize reorders around aggregates
    
    * feat: improved the inplace standardizer to account for nary operators and aggregate wrapping
    
    * chore: switching to postwalk for uniformity

--- src/finchlite/autoschedule/compiler.py
+++ src/finchlite/autoschedule/compiler.py
@@ -10,7 +10,7 @@ from finchlite.symbolic.traversal import PostOrderDFS
 
 from .. import finch_logic as lgc
 from .. import finch_notation as ntn
-from ..algebra import FType, ffuncs, ftypes
+from ..algebra import FinchOperator, FType, ffuncs, ftypes
 from ..compile.lower import make_extent
 from ..finch_assembly import AssemblyLibrary
 from ..finch_logic import LogicLoader, StatsFactory, TensorStats, compute_shape_vars
@@ -39,7 +39,7 @@ class PointwiseContext:
                         for arg in args
                     ),
                 )
-            case lgc.Table(lgc.Alias(_) as var, idxs):
+            case lgc.Table(lgc.Alias() as var, idxs):
                 return ntn.Unwrap(
                     ntn.Access(
                         self.ctx.slots[var],
@@ -94,6 +94,134 @@ class NotationContext:
             epilogue = ()
         self.epilogue = epilogue
 
+    def _lower_query_of_reorder(
+        self,
+        query_lhs: lgc.Alias,
+        op: FinchOperator,
+        arg: lgc.Table,
+        reorder_idxs: tuple[lgc.Field, ...],
+    ):
+        arg_dims = arg.dimmap(merge_shapes, self.shapes)
+        shapes_map = dict(zip(arg.idxs, arg_dims, strict=True))
+        shapes = {
+            idx: shapes_map.get(idx) or ntn.Literal(ftypes.intp(1))
+            for idx in arg.idxs + reorder_idxs
+        }
+        arg_types = arg.shape_type(self.shape_types)
+        shape_type_map = dict(zip(arg.idxs, arg_types, strict=True))
+        shape_type = {
+            idx: shape_type_map.get(idx) or ftypes.intp
+            for idx in arg.idxs + reorder_idxs
+        }
+        loop_idxs = []
+        remap_idxs = {}
+        out_idxs = iter(reorder_idxs)
+        out_idx = next(out_idxs, None)
+        new_idxs = []
+        for idx in arg.idxs:
+            loop_idxs.append(idx)
+            if idx == out_idx:
+                out_idx = next(out_idxs, None)
+                new_idxs.append(idx)
+            while (
+                out_idx in loop_idxs or out_idx not in arg.idxs
+            ) and out_idx is not None:
+                if out_idx in loop_idxs:
+                    new_idx = lgc.Field(gensym(f"{out_idx.name}_"))
+                    remap_idxs[new_idx] = out_idx
+                    loop_idxs.append(new_idx)
+                    new_idxs.append(new_idx)
+                else:
+                    loop_idxs.append(out_idx)
+                    new_idxs.append(out_idx)
+                out_idx = next(out_idxs, None)
+        while (out_idx in loop_idxs or out_idx not in arg.idxs) and out_idx is not None:
+            if out_idx in loop_idxs:
+                new_idx = lgc.Field(gensym(f"{out_idx.name}_"))
+                remap_idxs[new_idx] = out_idx
+                loop_idxs.append(new_idx)
+                new_idxs.append(new_idx)
+            else:
+                loop_idxs.append(out_idx)
+                new_idxs.append(out_idx)
+            out_idx = next(out_idxs, None)
+        loops = {
+            idx: ntn.Variable(
+                gensym(idx.name),
+                shape_type.get(idx) or shape_type[remap_idxs[idx]],
+            )
+            for idx in loop_idxs
+        }
+        ctx = PointwiseContext(self)
+        rhs = ctx(arg, loops)
+        lhs_access = ntn.Access(
+            self.slots[query_lhs],
+            ntn.Update(ntn.Literal(op)),
+            tuple(loops[idx] for idx in new_idxs),
+        )
+        body: ntn.NotationStatement = ntn.Increment(lhs_access, rhs)
+        for idx in reversed(loop_idxs):
+            stop = shapes.get(idx) or shapes[remap_idxs[idx]]
+            ext = ntn.Call(
+                ntn.Literal(make_extent),
+                (ntn.Literal(stop.result_type(0)), stop),
+            )
+            if idx in remap_idxs:
+                body = ntn.If(
+                    ntn.Call(
+                        ntn.Literal(ffuncs.eq),
+                        (loops[idx], loops[remap_idxs[idx]]),
+                    ),
+                    body,
+                )
+            body = ntn.Loop(
+                loops[idx],
+                ext,
+                body,
+            )
+
+        return body
+
+    def _lower_query_of_aggregate(
+        self,
+        query_lhs: lgc.Alias,
+        agg_op: FinchOperator,
+        agg_arg: lgc.Reorder,
+        agg_idxs: tuple[lgc.Field, ...],
+    ):
+        # Build a dict mapping fields to their shapes
+        arg_dims = agg_arg.dimmap(merge_shapes, self.shapes)
+        shapes_map = dict(zip(agg_arg.idxs, arg_dims, strict=True))
+        shapes = {idx: shapes_map.get(idx) or ntn.Literal(1) for idx in agg_arg.idxs}
+        arg_types = agg_arg.shape_type(self.shape_types)
+        shape_type_map = dict(zip(agg_arg.idxs, arg_types, strict=True))
+        shape_type = {
+            idx: shape_type_map.get(idx) or ftypes.intp for idx in agg_arg.idxs
+        }
+        loops = {
+            idx: ntn.Variable(gensym(idx.name), shape_type[idx]) for idx in agg_arg.idxs
+        }
+        ctx = PointwiseContext(self)
+        rhs = ctx(agg_arg.arg, loops)
+        lhs_access = ntn.Access(
+            self.slots[query_lhs],
+            ntn.Update(ntn.Literal(agg_op)),
+            tuple(loops[idx] for idx in agg_arg.idxs if idx not in agg_idxs),
+        )
+        body: ntn.NotationStatement = ntn.Increment(lhs_access, rhs)
+        for idx in reversed(agg_arg.idxs):
+            ext = ntn.Call(
+                ntn.Literal(make_extent),
+                (ntn.Literal(shape_type[idx](0)), shapes[idx]),
+            )
+            body = ntn.Loop(
+                loops[idx],
+                ext,
+                body,
+            )
+
+        return body
+
     def __call__(self, prgm: lgc.LogicStatement) -> ntn.NotationStatement:
         """
         Lower Finch Logic to Finch Notation. First we check for early
@@ -103,90 +231,8 @@ class NotationContext:
         match prgm:
             case lgc.Plan(bodies):
                 return ntn.Block(tuple(self(body) for body in bodies))
-            case lgc.Query(
-                lhs, lgc.Reorder(lgc.Table(lgc.Alias(_), idxs_1) as arg, idxs_2)
-            ):
-                arg_dims = arg.dimmap(merge_shapes, self.shapes)
-                shapes_map = dict(zip(idxs_1, arg_dims, strict=True))
-                shapes = {
-                    idx: shapes_map.get(idx) or ntn.Literal(ftypes.intp(1))
-                    for idx in idxs_1 + idxs_2
-                }
-                arg_types = arg.shape_type(self.shape_types)
-                shape_type_map = dict(zip(idxs_1, arg_types, strict=True))
-                shape_type = {
-                    idx: shape_type_map.get(idx) or ftypes.intp
-                    for idx in idxs_1 + idxs_2
-                }
-                loop_idxs = []
-                remap_idxs = {}
-                out_idxs = iter(idxs_2)
-                out_idx = next(out_idxs, None)
-                new_idxs = []
-                for idx in idxs_1:
-                    loop_idxs.append(idx)
-                    if idx == out_idx:
-                        out_idx = next(out_idxs, None)
-                        new_idxs.append(idx)
-                    while (
-                        out_idx in loop_idxs or out_idx not in idxs_1
-                    ) and out_idx is not None:
-                        if out_idx in loop_idxs:
-                            new_idx = lgc.Field(gensym(f"{out_idx.name}_"))
-                            remap_idxs[new_idx] = out_idx
-                            loop_idxs.append(new_idx)
-                            new_idxs.append(new_idx)
-                        else:
-                            loop_idxs.append(out_idx)
-                            new_idxs.append(out_idx)
-                        out_idx = next(out_idxs, None)
-                while (
-                    out_idx in loop_idxs or out_idx not in idxs_1
-                ) and out_idx is not None:
-                    if out_idx in loop_idxs:
-                        new_idx = lgc.Field(gensym(f"{out_idx.name}_"))
-                        remap_idxs[new_idx] = out_idx
-                        loop_idxs.append(new_idx)
-                        new_idxs.append(new_idx)
-                    else:
-                        loop_idxs.append(out_idx)
-                        new_idxs.append(out_idx)
-                    out_idx = next(out_idxs, None)
-                loops = {
-                    idx: ntn.Variable(
-                        gensym(idx.name),
-                        shape_type.get(idx) or shape_type[remap_idxs[idx]],
-                    )
-                    for idx in loop_idxs
-                }
-                ctx = PointwiseContext(self)
-                rhs = ctx(arg, loops)
-                lhs_access = ntn.Access(
-                    self.slots[lhs],
-                    ntn.Update(ntn.Literal(ffuncs.overwrite)),
-                    tuple(loops[idx] for idx in new_idxs),
-                )
-                body: ntn.NotationStatement = ntn.Increment(lhs_access, rhs)
-                for idx in reversed(loop_idxs):
-                    stop = shapes.get(idx) or shapes[remap_idxs[idx]]
-                    ext = ntn.Call(
-                        ntn.Literal(make_extent),
-                        (ntn.Literal(stop.result_type(0)), stop),
-                    )
-                    if idx in remap_idxs:
-                        body = ntn.If(
-                            ntn.Call(
-                                ntn.Literal(ffuncs.eq),
-                                (loops[idx], loops[remap_idxs[idx]]),
-                            ),
-                            body,
-                        )
-                    body = ntn.Loop(
-                        loops[idx],
-                        ext,
-                        body,
-                    )
-
+            case lgc.Query(lhs, lgc.Reorder(lgc.Table(lgc.Alias(), _) as arg, idxs_2)):
+                body = self._lower_query_of_reorder(lhs, ffuncs.overwrite, arg, idxs_2)
                 return ntn.Block(
                     (
                         ntn.Declare(
@@ -207,42 +253,11 @@ class NotationContext:
                 lgc.Aggregate(
                     lgc.Literal(op),
                     lgc.Literal(init),
-                    lgc.Reorder(arg, idxs_1) as arg_2,
+                    lgc.Reorder(arg, _) as arg_2,
                     idxs_2,
                 ),
             ):
-                # Build a dict mapping fields to their shapes
-                arg_dims = arg_2.dimmap(merge_shapes, self.shapes)
-                shapes_map = dict(zip(idxs_1, arg_dims, strict=True))
-                shapes = {idx: shapes_map.get(idx) or ntn.Literal(1) for idx in idxs_1}
-                arg_types = arg_2.shape_type(self.shape_types)
-                shape_type_map = dict(zip(idxs_1, arg_types, strict=True))
-                shape_type = {
-                    idx: shape_type_map.get(idx) or ftypes.intp for idx in idxs_1
-                }
-                loops = {
-                    idx: ntn.Variable(gensym(idx.name), shape_type[idx])
-                    for idx in idxs_1
-                }
-                ctx = PointwiseContext(self)
-                rhs = ctx(arg, loops)
-                lhs_access = ntn.Access(
-                    self.slots[lhs],
-                    ntn.Update(ntn.Literal(op)),
-                    tuple(loops[idx] for idx in idxs_1 if idx not in idxs_2),
-                )
-                body = ntn.Increment(lhs_access, rhs)
-                for idx in reversed(idxs_1):
-                    ext = ntn.Call(
-                        ntn.Literal(make_extent),
-                        (ntn.Literal(shape_type[idx](0)), shapes[idx]),
-                    )
-                    body = ntn.Loop(
-                        loops[idx],
-                        ext,
-                        body,
-                    )
-
+                body = self._lower_query_of_aggregate(lhs, op, arg_2, idxs_2)
                 return ntn.Block(
                     (
                         ntn.Declare(
@@ -250,6 +265,65 @@ class NotationContext:
                             ntn.Literal(init),
                             ntn.Literal(op),
                             (),
+                        ),
+                        body,
+                        ntn.Freeze(
+                            self.slots[lhs],
+                            ntn.Literal(op),
+                        ),
+                    )
+                )
+            case lgc.Query(
+                lhs,
+                lgc.Reorder(
+                    lgc.MapJoin(
+                        lgc.Literal(op),
+                        (
+                            lgc.Reorder(lgc.Table(lhs_1), idxs_1),
+                            lgc.Reorder(lgc.Table() as tbl, idxs_2),
+                        ),
+                    ),
+                    idxs_3,
+                ),
+            ) if lhs_1 == lhs and idxs_1 == idxs_3:
+                body = self._lower_query_of_reorder(lhs, op, tbl, idxs_2)
+                return ntn.Block(
+                    (
+                        ntn.Thaw(
+                            self.slots[lhs],
+                            ntn.Literal(op),
+                        ),
+                        body,
+                        ntn.Freeze(
+                            self.slots[lhs],
+                            ntn.Literal(op),
+                        ),
+                    )
+                )
+            case lgc.Query(
+                lhs,
+                lgc.Reorder(
+                    lgc.MapJoin(
+                        lgc.Literal(op),
+                        (
+                            lgc.Reorder(lgc.Table(lhs_1), idxs_1),
+                            lgc.Aggregate(
+                                lgc.Literal(op_1),
+                                lgc.Literal(init),
+                                lgc.Reorder() as agg_arg,
+                                agg_idxs,
+                            ),
+                        ),
+                    ),
+                    idxs_2,
+                ),
+            ) if lhs_1 == lhs and idxs_1 == idxs_2 and op_1 in (op, ffuncs.overwrite):
+                body = self._lower_query_of_aggregate(lhs, op_1, agg_arg, agg_idxs)
+                return ntn.Block(
+                    (
+                        ntn.Thaw(
+                            self.slots[lhs],
+                            ntn.Literal(op),
                         ),
                         body,
                         ntn.Freeze(
--- tests/test_logic_compiler.py
+++ tests/test_logic_compiler.py
@@ -10,6 +10,7 @@ from finchlite.finch_logic import (
     Aggregate,
     Alias,
     Field,
+    Literal,
     MapJoin,
     Plan,
     Produces,
@@ -74,6 +75,84 @@ def test_logic_compiler(file_regression):
     result = INTERPRET_NOTATION(plan, bindings)
 
     expected = np.matmul(
+        bindings[Alias(name="A0")].to_numpy(),
+        bindings[Alias(name="A1")].to_numpy(),
+        dtype=float,
+    )
+
+    finch_assert_equal(result[0].to_numpy(), expected)
+
+
+def test_logic_compiler_inplace(file_regression):
+    plan = Plan(
+        bodies=(
+            Query(
+                lhs=Alias(name="A2"),
+                rhs=Reorder(
+                    arg=MapJoin(
+                        op=Literal(ffuncs.add),
+                        args=(
+                            Reorder(
+                                arg=Table(
+                                    Alias("A2"), (Field(name="i0"), Field(name="i2"))
+                                ),
+                                idxs=(Field(name="i0"), Field(name="i2")),
+                            ),
+                            Aggregate(
+                                op=logic.Literal(val=ffuncs.add),
+                                init=logic.Literal(val=0),
+                                arg=Reorder(
+                                    arg=MapJoin(
+                                        op=logic.Literal(val=ffuncs.mul),
+                                        args=(
+                                            Table(
+                                                Alias(name="A0"),
+                                                (Field(name="i0"), Field(name="i1")),
+                                            ),
+                                            Table(
+                                                Alias(name="A1"),
+                                                (Field(name="i1"), Field(name="i2")),
+                                            ),
+                                        ),
+                                    ),
+                                    idxs=(
+                                        Field(name="i0"),
+                                        Field(name="i1"),
+                                        Field(name="i2"),
+                                    ),
+                                ),
+                                idxs=(Field(name="i1"),),
+                            ),
+                        ),
+                    ),
+                    idxs=(Field(name="i0"), Field(name="i2")),
+                ),
+            ),
+            Plan(
+                bodies=(Produces(args=(Alias(name="A2"),)),),
+            ),
+        ),
+    )
+
+    bindings = {
+        Alias(name="A0"): BufferizedNDArray.from_numpy(np.array([[1, 2], [3, 4]])),
+        Alias(name="A1"): BufferizedNDArray.from_numpy(np.array([[5, 6], [7, 8]])),
+        Alias(name="A2"): BufferizedNDArray.from_numpy(np.array([[1, 1], [1, 1]])),
+    }
+
+    program = NotationGenerator()(
+        plan, {var: ftype(val) for var, val in bindings.items()}
+    )
+
+    file_regression.check(
+        reset_name_counts(str(program)),
+        extension=".txt",
+        basename="test_logic_compiler_inplace_program",
+    )
+
+    result = INTERPRET_NOTATION(plan, bindings)
+
+    expected = np.ones_like(bindings[Alias(name="A2")].to_numpy()) + np.matmul(
         bindings[Alias(name="A0")].to_numpy(),
         bindings[Alias(name="A1")].to_numpy(),
         dtype=float,
